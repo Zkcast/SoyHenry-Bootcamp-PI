e7adbfa5e95b3ba437da364aff9ad4f1c9c6478c
Zkcast <castioniezequiel@gmail.com>
2022-10-21 13:48:16 -0300

    changes

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -3,12 +3,6 @@ const { Country, Activity } = require('../db');
 const router = Router();
 const axios = require('axios')
 
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const axios = require('axios')
-const { Country, Activity } = require('./src/db.js');
-const { activitiesOfWorld, CountriesWhere } = require('./extra-db/activities')
-
 // Para desarrollo >> 
 
 router.get('/', async (req, res) => {
@@ -57,63 +51,6 @@ router.get('/', async (req, res) => {
 //         res.status(404).send(error);
 //     }
 // })
-
-
-router.get('/loaddb', async(req, res) => {
-
-  const fetchCountries = await axios.get(`https://restcountries.com/v3.1/all`)
-  const allCountries = fetchCountries.data.map(country => {
-
-    const { name: { common }, translations: { spa }, flags: { svg }, maps, cca3, continents, capital, subregion, area, population } = country;
-
-    return {
-      name: common,
-      nameSpanish: spa.common ? spa.common : common,
-      flag: svg ? svg : "The server has not found a flag for this country",
-      id: cca3,
-      continent: continents[0],
-      capital: capital ? capital[0] : "Capital not found",
-      subregion: subregion ? subregion : "Subregion not found",
-      area: area ? area : 0,
-      population: parseInt(population),
-      maps: maps.googleMaps
-    }
-  })
-
-        allCountries.forEach(async (country) => {
-          try {
-            await Country.findOrCreate({
-              where: country
-            })
-          } catch (error) {
-            console.log(error)
-          }
-        })
-
-        activitiesOfWorld.forEach(async (act) => {
-          try {
-
-            await Activity.findOrCreate({
-              where: act
-            })  
-            try {
-              const finder = await Activity.findOne({ where: { name: act.name } })
-              await finder.setCountries(CountriesWhere(act.name))
-            } catch (error) {
-              console.log(error)
-            }
-
-          } catch (error) {
-            console.log(error)
-          }
-        })
-
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-});
-})
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params
