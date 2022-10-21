9f85bcc7a17c49135fad214bfd6dcef3870b7674
Zkcast <castioniezequiel@gmail.com>
2022-10-21 03:03:46 -0300

    changes

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,23 +1,54 @@
 const { Router } = require('express');
 const { Country, Activity } = require('../db');
 const router = Router();
+const axios = require('axios')
+
+// router.get('/', async (req, res) => {
+
+//     const {name} = req.query
+
+//     try {
+//         const allCountries = await Country.findAll({include: Activity});
+//         if (name) {
+//          const match = await allCountries.filter(country => country.name.toLowerCase().startsWith(name.toLowerCase()))
+//          match.length ? res.json(match) : res.status(404).send({ 'Error': 'No country matched with that name' })
+//         } else {
+//         return res.status(200).send(allCountries)}
+
+//     } catch (error) {
+//         res.status(404).send(error);
+//     }
+// })
+
 
 router.get('/', async (req, res) => {
 
-    const {name} = req.query
-
     try {
-        const allCountries = await Country.findAll({include: Activity});
-        if (name) {
-         const match = await allCountries.filter(country => country.name.toLowerCase().startsWith(name.toLowerCase()))
-         match.length ? res.json(match) : res.status(404).send({ 'Error': 'No country matched with that name' })
-        } else {
-        return res.status(200).send(allCountries)}
+        const fetchCountries = await axios.get(`https://restcountries.com/v3.1/all`)
+        const allCountries = fetchCountries.data.map(country => {
 
+            const { name: { common }, translations: { spa }, flags: { svg }, maps, cca3, continents, capital, subregion, area, population } = country;
+          
+            return {
+              name: common,
+              nameSpanish: spa.common ? spa.common : common,
+              flag: svg ? svg : "The server has not found a flag for this country",
+              id: cca3,
+              continent: continents[0],
+              capital: capital ? capital[0] : "Capital not found",
+              subregion: subregion ? subregion : "Subregion not found",
+              area: area ? area : 0,
+              population: parseInt(population),
+              maps: maps.googleMaps
+            }
+          })
+
+        return res.status(200).send(allCountries)
     } catch (error) {
         res.status(404).send(error);
     }
 })
+
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params
