78df8983d749d4acca78218612f202b159782b21
Zkcast <castioniezequiel@gmail.com>
2022-10-21 02:21:56 -0300

    changes

--- api/src/db.js
+++ api/src/db.js
@@ -4,38 +4,44 @@ const fs = require('fs');
 const path = require('path');
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
-let sequelize = 
-  process.env.NODE_ENV === "production"
-    ? new Sequelize ({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-    })
-    : new Sequelize(
-      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
-      { logging: false, native: false }
-    );
+// let sequelize = 
+//   process.env.NODE_ENV === "production"
+//     ? new Sequelize ({
+//         database: DB_NAME,
+//         dialect: "postgres",
+//         host: DB_HOST,
+//         port: 5432,
+//         username: DB_USER,
+//         password: DB_PASSWORD,
+//         pool: {
+//           max: 3,
+//           min: 1,
+//           idle: 10000,
+//         },
+//         dialectOptions: {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//           },
+//           keepAlive: true,
+//         },
+//         ssl: true,
+//     })
+//     : new Sequelize(
+//       `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+//       { logging: false, native: false }
+//     );
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
+
+const sequelize = new Sequelize(`postgres://mugqjtlngzhrep:ca944304b9700b1bcb7a9d71dfb264dc49683a5046d6a60babd406a7d8196cfb@ec2-35-170-146-54.compute-1.amazonaws.com:5432/dc43u9dfjudsr1
+`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
 const basename = path.basename(__filename);
 
