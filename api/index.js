c21a9a83938836d8667dfb6d835f14ce9adaf3e6
Zkcast <castioniezequiel@gmail.com>
2022-10-21 20:23:14 -0300

    changes for deploy2

--- api/index.js
+++ api/index.js
@@ -90,7 +90,7 @@ const newDB = async () => {
 
 newDB()
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
